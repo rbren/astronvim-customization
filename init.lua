5efc990ec3e20a6a5149b6843ea6974c0a6bf34e
Robert Brennan <accounts@rbren.io>
2023-03-13 12:27:46 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "elford",
+  colorscheme = "elflord",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
