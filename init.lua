bdc1bd6d9d4b12e56688abf2e17dbd671954c1a6
Robert Brennan <contact@rbren.io>
2023-03-14 22:18:51 -0400

    remove underscore keyword

--- init.lua
+++ init.lua
@@ -66,6 +66,8 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    vim.cmd(':set iskeyword-=_')
+
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
