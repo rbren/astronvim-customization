d5ca2740d63b27e2ae58645d052cfd499d0645de
Robert Brennan <contact@rbren.io>
2023-03-14 22:20:48 -0400

    highlight trailing whitespace

--- init.lua
+++ init.lua
@@ -60,13 +60,15 @@ return {
       },
     },
   },
-  
+
 
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
-    vim.cmd(':set iskeyword-=_')
+    vim.cmd(":set iskeyword-=_")
+    vim.cmd("hi ExtraWhitespace ctermbg=red guibg=red")
+    vim.cmd("match ExtraWhitespace /\\s\\+$/")
 
     -- Set up custom filetypes
     -- vim.filetype.add {
